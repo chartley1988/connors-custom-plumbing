f738204dd50544b0bdc1a5aff49fbbf551ae001b
Carson Hartley <chartley1988@gmail.com>
2023-01-28 10:27:27 -0700

    Config edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://chartley1988.github.io',
-    base: '/connors-custom-plumbing'
+    base: '/connors-custom-plumbing',
 });
